8d6b31f6f7849e69c1824ce8d4762a4cb9d2a267
HoverfishOTS <125647846+HoverfishOTS@users.noreply.github.com>
2026-02-24 11:33:07 -0800

    Update app.py
    corrected port

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def add_entry():
     return jsonify(entry), 201
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
